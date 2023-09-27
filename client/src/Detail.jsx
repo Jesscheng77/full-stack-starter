e75966331305de3058dfc89d3233adf9b3f0c085
Jessica Cheng <jessicacheng8371@gmail.com>
2023-09-26 20:29:23 -0700

    Added Item and Detail components

---
+++ client/src/Detail.jsx
@@ -0,0 +1,8 @@
+function Detail() {
+    return(
+    <main className="container">
+        <h1>Detail</h1>
+</main>)
+}
+
+export default Detail;
---
+++ client/src/Item.jsx
@@ -0,0 +1,8 @@
+import { Link } from 'react-router-dom';
+
+function Item({title}) {
+    return <div>{title}<Link to="/detail">Link</Link>
+    </div>;
+}
+
+export default Item; 
