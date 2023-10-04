dc96dded68cbd1dd138c149fb6564355ff6db8a2
Jessica Cheng <jessicacheng8371@gmail.com>
2023-10-03 20:58:57 -0700

    Connect database information to code

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useStaticContext } from './StaticContext';
 
 import Item from './Item';
-
 function Home() {
+
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const token = 'patKqXPaSWJJgckAx.4cc2daa28db95ba77db5932c2528bb277aebea8b18426b3e1a23513cb521a1d9';
+    const url = 'https://api.airtable.com/v0/appjNeZQnrvy9hYsq/Restaurants?view=Grid%20view'
+    fetch(url, {
+      
+      headers: { Authorization: `Bearer ${token}`}
+
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      setData(data)});
+    
+
+  }, []);
   const staticContext = useStaticContext();
   return (
     <>
@@ -12,10 +30,10 @@ function Home() {
       </Helmet>
       <main className="container">
         <h1>Home</h1>
-        <Item title="Title 1" />
-        <Item title="Title 2" />
-        <Item title="Title 3" />
         
+        <div className="row" >
+        {data?.records.map((record) => <Item key={record.id} id={record.id} fields={record.fields} />) }
+        </div>
       </main>
     </>
   );
--- client/src/Item.jsx
+++ client/src/Item.jsx
@@ -1,8 +1,22 @@
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
-function Item({title}) {
-    return <div>{title}<Link to="/detail">Link</Link>
-    </div>;
+
+function Item({ id, fields }) {
+    return (<div className="col-xl-4 d-flex justify-content-center">
+        <div className="card cardImageText">
+            <img src={fields.images[0].url}/>
+            <div className="card-body">
+             <Link to={`/detail/${id}`}><h2 className="card-title card-key">{fields.Name}</h2></Link>
+            </div>
+        </div>
+    </div >
+        // <div className ="card mb-3">
+        //     {title} <Link to="/detail">Link</Link>
+        //     </div>
+    );
+
+
 }
+
 
 export default Item; 
